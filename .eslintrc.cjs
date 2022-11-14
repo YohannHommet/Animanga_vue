b2fdd2659d75db41b736cca4d140dce7cccc549c
YohannHommet <yohann.hommet@outlook.fr>
2022-11-15 00:20:01 +0100

    feat: add axios

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,18 +21,9 @@ module.exports = {
   },
   plugins: ["vue", "@typescript-eslint"],
   rules: {
-    "no-unused-vars": "warn",
-    "no-empty": "warn",
-    "no-empty-function": "warn",
-    "no-useless-constructor": "warn",
-    "no-useless-return": "warn",
-    // allow paren-less arrow functions
-    "arrow-parens": "off",
     // allow debugger during development
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     // allow console during development
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    // double quotes
-    quotes: ["error", "double"],
   },
 }
