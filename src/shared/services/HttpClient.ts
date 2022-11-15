f12ea28f07672e7a16ed94e9a092ada4b03524db
YohannHommet <yohann.hommet@outlook.fr>
2022-11-16 00:19:58 +0100

    feat: update HttpClient.ts with axios

--- src/shared/services/HttpClient.ts
+++ src/shared/services/HttpClient.ts
@@ -1,10 +1,29 @@
-import axios, { AxiosInstance, AxiosResponse } from 'axios';
+import axios from 'axios';
+import type { AxiosInstance } from "axios";
+import type { AxiosResponse } from "axios";
 
-declare module "axios" {
+declare module 'axios' {
   interface AxiosResponse<T = any> extends Promise<T> {}
 }
 
 abstract class HttpClient {
-
   protected readonly instance: AxiosInstance;
+
+  protected constructor(baseURL: string) {
+    this.instance = axios.create({
+      baseURL,
+    });
+
+    this.instance.interceptors.response.use(this.handleSuccess, this.handleError);
+  }
+
+  private handleSuccess({ data }: AxiosResponse): AxiosResponse {
+    return data;
+  }
+
+  private handleError(error: any): Promise<any> {
+    return Promise.reject(error);
+  }
 }
+
+export default HttpClient;
