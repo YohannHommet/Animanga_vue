3488d2bec2381ca4ef7bddd3c57acd76d1ed3254
YohannHommet <yohann.hommet@outlook.fr>
2022-12-21 22:29:53 +0100

    feat: add image overlay on click

--- src/views/anime.view.vue
+++ src/views/anime.view.vue
@@ -1,13 +1,16 @@
 <template>
 <main id="anime">
-  <h1 class="anime_title">{{ anime.title }}</h1>
+  <h1 class="anime_title">{{ anime?.title }}</h1>
   <div class="anime_card">
     <div class="anime_card__img">
-      <img :src="anime.image" alt="anime image">
+      <img :src="anime?.image" :alt="anime.title" @click="showOverlay = !showOverlay">
     </div>
     <div class="anime_card__description">
-      <p>{{ anime.synopsis }}</p>
+      <p>{{ anime?.synopsis }}</p>
     </div>
+  </div>
+  <div class="overlay_container" v-if="showOverlay">
+    <img class="overlay_image" :src="anime.image" :alt="anime.title" @click="showOverlay = !showOverlay">
   </div>
 </main>
 </template>
@@ -21,14 +24,14 @@ export default {
 <script lang="ts" setup>
 import { ref } from "vue";
 import { RouteLocationNormalizedLoaded, useRoute } from "vue-router";
-
 import { IAnime } from "@/core/interfaces/anime.interface";
 import { AnimeRepository } from "@/shared/services/anime.repository";
 
 const animeRepository: AnimeRepository = AnimeRepository.getInstance();
 const route: RouteLocationNormalizedLoaded = useRoute();
-const anime = ref<IAnime>();
 const id = ref(route.params.animeId);
+const anime = ref<IAnime>();
+const showOverlay = ref<Boolean>(false);
 
 const getAnime: (id?: string) => Promise<void> = async () => {
   try {
@@ -49,6 +52,25 @@ getAnime();
   margin: 1rem 0;
 }
 
+.overlay_container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+}
+
+.overlay_image {
+  max-width: 100%;
+  max-height: 100%;
+  cursor: pointer;
+}
+
 .anime_card__img {
   height: 200px;
   padding: 0.6rem;
@@ -59,7 +81,7 @@ getAnime();
 
 .anime_card__img img {
   height: 100%;
-  border-top-left-radius: 10px;
+  border-radius: 5px;
 }
 
 .anime_card__description {
