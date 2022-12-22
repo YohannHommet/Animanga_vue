09c13b50b5b148a35e480501ed331b17f1dbbcfb
YohannHommet <yohann.hommet@outlook.fr>
2022-12-22 22:02:02 +0100

    feat: update router

--- src/core/router/index.ts
+++ src/core/router/index.ts
@@ -1,35 +1,43 @@
-import type { Router } from 'vue-router';
-import { createRouter, createWebHistory } from 'vue-router';
+import type { Router } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 const router: Router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: () => import('../../views/home.view.vue'),
+      path: "/",
+      name: "home",
+      component: () => import("../../views/home.view.vue")
     },
     {
-      path: '/anime/:animeId',
-      name: 'anime',
-      component: () => import('../../views/anime.view.vue'),
+      path: "/anime/:animeId",
+      name: "anime",
+      component: () => import("../../views/anime.view.vue"),
+      props: true,
+      beforeEnter: (to, from, next) => {
+        const isValid = Number.isInteger(Number(to.params.animeId));
+
+        !isValid
+          ? next({ name: "home" })
+          : next();
+      }
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../../views/about.view.vue'),
+      path: "/about",
+      name: "about",
+      component: () => import("../../views/about.view.vue")
     },
     {
-      path: '/:catchAll(.*)',
-      name: 'not-found',
-      component: () => import('../../views/not-found.view.vue'),
-    },
-  ],
+      path: "/:catchAll(.*)",
+      name: "not-found",
+      component: () => import("../../views/not-found.view.vue")
+    }
+  ]
 });
 
 router.beforeEach((to, from, next) => {
   const title: string = to.name as string;
-  document.title = title.charAt(0).toUpperCase() + title.slice(1).replace(/[-_]/g, ' ');
+  document.title = title.charAt(0).toUpperCase() + title.slice(1).replace(/[-_]/g, " ");
   next();
 });
 
