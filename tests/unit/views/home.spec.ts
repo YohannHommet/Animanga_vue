4154457b6e95727b1f19784d3929b8b4ec260c60
YohannHommet <yohann.hommet@outlook.fr>
2022-12-22 22:46:07 +0100

    feat: update home tests

--- tests/unit/views/home.spec.ts
+++ tests/unit/views/home.spec.ts
@@ -1,14 +1,21 @@
 import { describe, expect, it } from "vitest";
-
 import { mount } from "@vue/test-utils";
 import Home from "@/views/home.view.vue";
 
-describe("home view", () => {
-  it("renders properly",
-    () => {
-      const wrapper = mount(Home);
+describe("Home", () => {
+  it("should renders", () => {
+    const wrapper = mount(Home);
+    expect(wrapper.vm).toBeTruthy();
+  });
 
-      expect(wrapper.vm).toBeTruthy();
+  it("should be named home-view", function() {
+    const wrapper = mount(Home);
+    expect(wrapper.vm.$options.name).toBe("home-view");
+  });
 
-    });
+  it("should have an async function getTopAnimeList", function() {
+    const wrapper = mount(Home);
+    expect(wrapper.vm.getTopAnimeList).toBeInstanceOf(Function);
+  });
+
 });
