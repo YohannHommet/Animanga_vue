45cad8ab8bcf4243eccbe798aedd3520b5aad6e4
YohannHommet <yohann.hommet@outlook.fr>
2023-01-03 00:47:54 +0100

    feat: quick updates

--- src/views/anime.view.vue
+++ src/views/anime.view.vue
@@ -22,11 +22,11 @@ export default {
 </script>
 
 <script lang="ts" setup>
-import { defineProps } from "vue";
 import { ref } from "vue";
-import { AnimeRepository } from "@/shared/services/anime.repository";
-import type { IAnime } from "@/core/interfaces/anime.interface";
+import { AnimeRepository } from "@/shared/repositories/anime.repository";
+import type { IAnime } from "@/shared/interfaces/anime.interface";
 
+// Props
 const props = defineProps<{
   animeId: {
     type: string;
@@ -34,62 +34,63 @@ const props = defineProps<{
   };
 }>();
 
+// Variables
 const animeRepository: AnimeRepository = AnimeRepository.getInstance();
 const anime = ref<IAnime>();
 const showOverlay = ref<Boolean>(false);
 
+// Methods
 const getAnime: () => Promise<void> = async () => {
-  try {
-    anime.value = await animeRepository.getById(+props.animeId);
-  } catch (error) {
-    console.error(error);
-  }
+  anime.value = await animeRepository.getById(+props.animeId);
 };
 
 getAnime();
 </script>
 
-<style scoped>
-.anime_card {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin: 1rem 0;
-}
+<style lang="scss" scoped>
+#anime {
+  .anime_card {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 1rem 0;
 
-.overlay_container {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background-color: rgba(0, 0, 0, 0.3);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1000;
-}
+    .anime_card__img {
+      height: 200px;
+      padding: 0.6rem;
+      box-shadow: 0 0 12px 0 rgba(0, 0, 0, 0.2);
+      border-radius: 3px;
+      transition: all 0.3s ease-in-out;
 
-.overlay_image {
-  max-width: 100%;
-  max-height: 100%;
-  cursor: pointer;
-}
+      img {
+        height: 100%;
+        border-radius: 5px;
+        cursor: pointer;
+      }
+    }
 
-.anime_card__img {
-  height: 200px;
-  padding: 0.6rem;
-  box-shadow: 0 0 12px 0 rgba(0, 0, 0, 0.2);
+    .anime_card__description {
+      padding: 1.2rem;
+    }
+  }
 
-  border-radius: 3px;
-}
+  .overlay_container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1000;
 
-.anime_card__img img {
-  height: 100%;
-  border-radius: 5px;
-}
-
-.anime_card__description {
-  padding: 1.2rem;
+    .overlay_image {
+      max-width: 100%;
+      max-height: 100%;
+      cursor: pointer;
+    }
+  }
 }
 </style>
--- tests/unit/views/home.spec.ts
+++ tests/unit/views/home.spec.ts
@@ -3,19 +3,23 @@ import { mount } from "@vue/test-utils";
 import Home from "@/views/home.view.vue";
 
 describe("Home", () => {
+
   it("should renders", () => {
-    const wrapper = mount(Home);
+    const wrapper = mount(Home, {
+      global: {
+        stubs: ["router-link"]
+      }
+    });
     expect(wrapper.vm).toBeTruthy();
   });
 
   it("should be named home-view", function() {
-    const wrapper = mount(Home);
+    const wrapper = mount(Home, {
+      global: {
+        stubs: ["router-link"]
+      }
+    });
     expect(wrapper.vm.$options.name).toBe("home-view");
-  });
-
-  it("should have an async function getTopAnimeList", function() {
-    const wrapper = mount(Home);
-    expect(wrapper.vm.getTopAnimeList).toBeInstanceOf(Function);
   });
 
 });
