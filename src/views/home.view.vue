bf9ae2165d41b65bbe2282a4303036e79edb14d1
YohannHommet <yohann.hommet@outlook.fr>
2023-01-03 00:41:10 +0100

    feat: add loader

--- src/views/home.view.vue
+++ src/views/home.view.vue
@@ -1,9 +1,13 @@
 <template>
 <main id="home">
+  <div v-if="isLoading">
+    <img alt="loading" class="spinner" src="/spinner.gif">
+  </div>
   <h1 class="anime_title">Top Anime list</h1>
-  <ul>
+  <ul v-if="!isLoading">
     <li v-for="anime in animeList" :key="anime.id">
-      <router-link :to="{ name: 'anime', params: { animeId: anime.id } }" class="anime">{{ anime.title }}</router-link>
+      <router-link :to="{ name: 'anime', params: { animeId: +anime.id, animeTitle: slugify(anime.title) } }" class="anime">{{ anime.title }}
+      </router-link>
     </li>
   </ul>
   <!-- pagination -->
@@ -21,7 +25,8 @@
   <div class="anime__card">
     <ul>
       <li v-for="search in animeSearch" :key="search.id">
-        <router-link :to="{ name: 'anime', params: { animeId: search.id } }" class="search">{{ search.title }}</router-link>
+        <router-link :to="{ name: 'anime', params: { animeId: +search.id, animeName: search.title } }" class="search">{{ search.title }}
+        </router-link>
       </li>
     </ul>
     <!-- pagination -->
@@ -41,33 +46,45 @@ export default {
 
 <script lang="ts" setup>
 import { onMounted, ref } from "vue";
-import { AnimeRepository } from "@/shared/services/anime.repository";
-import type { IDataList } from "@/core/interfaces/anime-list.interface";
-import type { IAnime } from "@/core/interfaces/anime.interface";
-import type { IPagination } from "@/core/interfaces/pagination.interface";
+import { AnimeRepository } from "@/shared/repositories/anime.repository";
+import type { IDataList } from "@/shared/interfaces/anime-list.interface";
+import type { IAnime } from "@/shared/interfaces/anime.interface";
+import type { IPagination } from "@/shared/interfaces/pagination.interface";
 
 // Variables
 const animeRepository: AnimeRepository = AnimeRepository.getInstance();
+const isLoading = ref<Boolean>(false);
 const animeList = ref<IAnime[]>();
 const topPagination = ref<IPagination>();
 const searchPagination = ref<IPagination>();
 const animeSearch = ref<IAnime[]>();
 const querySearch = ref("");
 
-
 // Methods
-async function getTopAnimeList(page: number = 1) {
+const slugify = (text: string) => {
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(/[^\w-]+/g, "") // Remove all non-word chars
+    .replace(/--+/g, "-") // Replace multiple - with single -
+    .replace(/^-+/, "") // Trim - from start of text
+    .replace(/-+$/, ""); // Trim - from end of text
+};
+
+async function getTopAnimeList(page: number = 1): Promise<void> {
+  isLoading.value = true;
+
   const params = {
     page: page.toString()
   };
   const searchParams = new URLSearchParams(params);
-  try {
-    const { data, pagination }: IDataList<IAnime> = await animeRepository.getTopList(searchParams);
-    animeList.value = data;
-    topPagination.value = pagination;
-  } catch (error) {
-    console.error(error);
-  }
+
+  const { data, pagination }: IDataList<IAnime> = await animeRepository.getTopList(searchParams);
+  animeList.value = data;
+  topPagination.value = pagination;
+
+  isLoading.value = false;
 }
 
 const search: () => Promise<void> = async () => {
@@ -76,14 +93,10 @@ const search: () => Promise<void> = async () => {
   };
   const searchParams = new URLSearchParams(params);
 
-  try {
-    const { data, pagination }: IDataList<IAnime> = await animeRepository.getBySearch(searchParams);
+  const { data, pagination }: IDataList<IAnime> = await animeRepository.getBySearch(searchParams);
 
-    animeSearch.value = data;
-    searchPagination.value = pagination;
-  } catch (error) {
-    console.error(error);
-  }
+  animeSearch.value = data;
+  searchPagination.value = pagination;
 };
 
 const nextPage: () => Promise<void> = async () => {
@@ -106,14 +119,23 @@ onMounted(() => {
 });
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .anime {
   cursor: pointer;
   transition: 0.2s;
+  color: var(--color-link);
+
+  &:hover {
+    color: var(--color-hover);
+  }
 }
 
-.anime:hover {
-  color: black;
-  font-weight: bold;
+.spinner {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 60px;
+  height: 60px;
 }
 </style>
