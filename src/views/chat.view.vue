e2c31d2773b468ba095af2fbd853d9a0d0aa65fe
YohannHommet <yohann.hommet@outlook.fr>
2023-01-03 00:39:40 +0100

    feat: add chat-view for chat gpt tests

---
+++ src/views/chat.view.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <!--  Make a button that will trigger a function to scrap an api endpoint -->
+    <button class="btn btn-primary" @click="(scrap)">Scrap</button>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
