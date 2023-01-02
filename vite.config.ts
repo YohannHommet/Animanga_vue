cf6dbea7638b40e9fe852dfcbd5b0833c2af8229
YohannHommet <yohann.hommet@outlook.fr>
2023-01-03 00:35:55 +0100

    feat: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,9 @@
-/// <reference types="vitest" />
-
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({
-    template: {
-      compilerOptions: {
-        isCustomElement: (tag) => ["router-link", "router-view"].includes(tag)
-      }
-    }
-  })],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
