b3a854804a281f84443b2956fa19d4760eeed53e
YohannHommet <yohann.hommet@outlook.fr>
2023-01-03 00:40:05 +0100

    feat: add link to nav

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
-</script>
+import { RouterLink, RouterView } from "vue-router";</script>
 
 <template>
-<header>
+<header id="header">
   <div class="wrapper">
-    <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
+    <nav class="header_nav">
+      <router-link class="header_nav__link" to="/">Home</router-link>
+      <router-link class="header_nav__link" to="/about">About</router-link>
+      <router-link class="header_nav__link" to="/chat">Chat</router-link>
     </nav>
   </div>
 </header>
@@ -15,37 +15,47 @@ import { RouterLink, RouterView } from 'vue-router';
 <router-view />
 </template>
 
-<style scoped>
-  header {
-    line-height: 1.5;
-  }
-  nav {
-    width: 100%;
-    font-size: 12px;
-    text-align: center;
-    padding: 2rem 0;
-  }
+<style lang="scss" scoped>
+#header {
+  line-height: 1.5;
+}
 
-  nav a.router-link-exact-active {
-    color: #000000;
-  }
+.header_nav {
+  width: 100%;
+  text-align: center;
+  padding: 2rem 0;
 
-  nav a:visited {
-    color: #4c4c4c;
-  }
-
-  nav a.router-link-exact-active:hover, nav a:hover {
-    opacity: 0.8;
-  }
-
-  nav a {
+  .header_nav__link {
     display: inline-block;
     padding: 0 1rem;
+    font-size: 1.8rem;
+    text-decoration: none;
     border-left: 1px solid var(--color-border);
-    color: #4c4c4c;
+
+    &:first-child {
+      border-left: none;
+    }
+
+    &:hover {
+      color: var(--color-hover);
+    }
+
+    &:active {
+      color: var(--color-link);
+    }
+
+    &:visited {
+      color: var(--color-text);
+    }
+
+    &.router-link-exact-active:hover, &:hover {
+      color: var(--color-hover);
+    }
+
+    &.router-link-exact-active {
+      color: var(--color-hover);
+    }
   }
 
-  nav a:first-of-type {
-    border: 0;
-  }
+}
 </style>
