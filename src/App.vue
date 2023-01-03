fa9704af1b8267912ee9a54d58fd8be23b0c136f
YohannHommet <yohann.hommet@outlook.fr>
2023-01-04 00:00:57 +0100

    feat: update app.vue with new component and layout

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,15 @@
 <template>
-<header id="header">
-  <div class="wrapper">
-    <nav class="header_nav">
-      <router-link class="header_nav__link" to="/">Home</router-link>
-      <router-link class="header_nav__link" to="/about">About</router-link>
-      <router-link class="header_nav__link" to="/chat">Chat</router-link>
-    </nav>
-  </div>
-</header>
+<navbar-component />
 
+<div class="container">
+  <slider-component />
   <router-view />
+</div>
 </template>
 
 <script lang="ts" setup>
-import { RouterLink, RouterView } from "vue-router";</script>
+import { RouterView } from "vue-router";
+import SliderComponent from "@/components/slider.vue";
+import NavbarComponent from "@/components/navbar.vue";</script>
 
-
-<style lang="scss" scoped>
-#header {
-  line-height: 1.5;
-}
-
-.header_nav {
-  width: 100%;
-  text-align: center;
-  padding: 2rem 0;
-
-  .header_nav__link {
-    display: inline-block;
-    padding: 0 1rem;
-    font-size: 1.8rem;
-    text-decoration: none;
-    border-left: 1px solid var(--color-border);
-
-    &:first-child {
-      border-left: none;
-    }
-
-    &:hover {
-      color: var(--color-hover);
-    }
-
-    &:active {
-      color: var(--color-link);
-    }
-
-    &:visited {
-      color: var(--color-text);
-    }
-
-    &.router-link-exact-active:hover, &:hover {
-      color: var(--color-hover);
-    }
-
-    &.router-link-exact-active {
-      color: var(--color-hover);
-    }
-  }
-
-}
-</style>
+<style lang="scss" scoped></style>
